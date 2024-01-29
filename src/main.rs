1526e041bd2a53c8f608e250be89dfd4a3ebd046
perkzen <perko.domen@gmail.com>
2024-01-29 12:05:35 +0100

    feat: remove swagger

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,13 @@ use std::io::Error;
 use std::net::{SocketAddr};
 use tokio::net::TcpListener;
 use utoipa::{OpenApi};
-use utoipa_swagger_ui::SwaggerUi;
+use utoipa_rapidoc::RapiDoc;
 use crate::api::health::health_check;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     #[derive(OpenApi)]
     #[openapi(
-    servers((url = "http://localhost:3000", description = "Local server")),
     paths(api::health::health_check),
     components(schemas(api::health::Health)),
     info(title = "Rust Shortener", description = "Rust URL Shortener")
@@ -29,12 +28,12 @@ async fn main() -> Result<(), Error> {
     let port = env::var("PORT").unwrap_or_else(|_| "3000".to_string());
 
     let app = Router::new()
-        .merge(SwaggerUi::new("/swag").url("/api-docs.json", ApiDoc::openapi()))
+        .merge(RapiDoc::with_openapi("/api-docs/openapi.json", ApiDoc::openapi()).path("/docs"))
         .route("/", get(health_check));
 
     let address = SocketAddr::from(([0, 0, 0, 0], port.parse::<u16>().unwrap()));
     let listener = TcpListener::bind(&address).await?;
-    println!("Listening on http://{}", address);
+    println!("Documentation available on http://{}/docs", address);
     axum::serve(listener, app.into_make_service()).await
 }
 
