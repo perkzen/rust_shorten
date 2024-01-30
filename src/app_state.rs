8cc2b86ef81e7a5fd47dd4f5b2e90a399079fdfb
perkzen <perko.domen@gmail.com>
2024-01-30 21:31:55 +0100

    fix: make code cleaner

--- src/app_state.rs
+++ src/app_state.rs
@@ -11,8 +11,12 @@ pub struct AppState {
 impl AppState {
     pub async fn new(redis_url: String) -> Self {
         let redis_client = Client::open(redis_url).expect("Failed to connect to Redis");
+
+        let connection = redis_client.get_async_connection().await
+            .expect("Failed to establish an async connection to Redis.");
+
         Self {
-            redis: Arc::new(Mutex::new(redis_client.get_async_connection().await.unwrap())),
+            redis: Arc::new(Mutex::new(connection)),
         }
     }
 }
